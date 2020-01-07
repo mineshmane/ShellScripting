8b53b2f0551f785dc7ec045dd7fcc6c52837eecd
mineshmane <mineshmane94@gmail.com>
2020-01-07 11:59:33 +0530

    commit

--- set3/minmax.sh
+++ set3/minmax.sh
@@ -1,12 +1,20 @@
 #!/bin/bash -x
-newNumber=0;
-for i in 1 2 3 4 5
-do 
-	number=$((RANDOM%999))
-		
-	if [ $newNumber -ge $number ]
-	then
-		$newNumber=$number
+min=1000
+max=0
+	for i in 1 2 3 4 5 
+do
+	num=$((RANDOM%900+100))
+		if [ $max -lt $num ]
+		then
+			max=$num
+		echo $max
+		fi
+		if [ $min -gt $num ]
+	then 
+	min=$num
 	fi
-		echo $newNumber
+	echo min
 done
+	echo "min value :"$min
+	echo "max value :"$max
+
