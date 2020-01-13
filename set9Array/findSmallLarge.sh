8bdbfacdefb3fd4453633076f9d6f7f89e85f9b5
mineshmane <mineshmane94@gmail.com>
2020-01-13 18:44:58 +0530

    Refactor:changes in find large n small number

--- set9Array/findSmallLarge.sh
+++ set9Array/findSmallLarge.sh
@@ -3,5 +3,21 @@ for (( i=0; i<=10; i++ ))
 	do
 	 number[$i]=$((RANDOM%900+100))
 	done
-	echo "${number[@]}"
-
+		echo "${number[@]}"
+		ssmall="${number[0]}"
+		small="${number[0]}"
+		echo $small $ssmall
+		for (( i=0; i<10; i++ ))
+			do
+			echo "samll "$small " large " $ssmall
+			echo ${number[$i]}
+			if [ ${number[$i]} -lt  $small ]
+			then
+			small=${number[$i]}
+			elif [ ${number[$i]}  -gt $ssmall ]
+			then
+			ssmall=${number[$i]}
+			fi
+			done
+			echo "smallest numberr" $small
+			echo "largest numberr" $ssmall
